6e8af0dec07c8becdcbeb2da27186c1ead107cf3
Kyuho-Han <rodius93@naver.com>
2021-08-13 15:13:09 +0900

    08.11

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,21 +15,25 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountpolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
+
         return new RateDiscountpolicy();
     }
 }
